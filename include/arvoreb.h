f27a296ed65c263422eebefae92cc9ef7e45cab2
Jeanna <80374518+jeannads@users.noreply.github.com>
2026-07-11 18:29:30 -0300

    Add files via upload

---
+++ include/arvoreb.h
@@ -0,0 +1,12 @@
+#ifndef ARVOREB_H
+#define ARVOREB_H
+
+void inserir_arvore(int id_filme, long offset_dado);
+
+long buscar_offset_dado(int id_filme);
+
+void remover_arvore(int id_filme);
+
+void menu_exibir_arvore();
+
+#endif
